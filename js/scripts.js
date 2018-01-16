b678d40946ae195171e9f4f8dd77b0957133d10c
Panatda Inthala <panatdainthala@gmail.com>
2018-01-15 20:17:34 -0800

    updated the html to add radio button and updated js to add branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-//  Business (or back-end) logic:
-
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -10,20 +8,28 @@ var subtract = function(number1, number2) {
 
 var multiply = function(number1, number2) {
   return number1 * number2;
-}
+};
 
 var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-// Everything below this line is user interface (or front-end) logic:
-
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-  var result = add(number1, number2);
-  $("#output").text(result);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
   });
 });
